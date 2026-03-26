428bdf1cd2b968527805b8393021530c7538e50b
am0nipro01 <amonipro01@gmail.com>
2026-03-26 13:11:30 +0100

    fix: reservation page bottom spacing and scroll to top on navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
+import ScrollToTop from './components/ScrollToTop'
 import Home from './pages/Home'
 import Reservation from './pages/Reservation'
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
